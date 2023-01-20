e95d42e10429d0a85ebcc0d687da80fefc967d9b
Shakif-a <84608878+Shakif-a@users.noreply.github.com>
2023-01-20 11:13:52 +1100

    quaternion to eigen testing

---
+++ src/quaternion_playground_v2.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+# Just to have fun with quaternions, visualize them for my own understanding of how they work
+import rospy
+import tf2_ros
+from geometry_msgs.msg import Quaternion, Vector3, Transform, TransformStamped
+from fiducial_msgs.msg import FiducialTransformArray
+from tf.transformations import *
+
+def vector3_add(vector1, vector2):
+    vector_r = Vector3()
+    vector_r.x=vector1.x + vector2.x
+    vector_r.y=vector1.y + vector2.y
+    vector_r.z=vector1.z + vector2.z
+
+    return vector_r
+
+def quat_to_array(quaternion):
+    array = [0,0,0,0]
+    array[0] = quaternion.x
+    array[1] = quaternion.y
+    array[2] = quaternion.z
+    array[3] = quaternion.w
+
+    return array
+
+def array_to_quat(array):
+    quaternion = Quaternion()
+
+    quaternion.x = array[0]
+    quaternion.y = array[1]
+    quaternion.z = array[2] 
+    quaternion.w = array[3]
+
+    return quaternion
+
+def reverse_translate(translation):
+    result = Vector3()
+    result.x = -translation.x
+    result.y = -translation.y
+    result.z = -translation.z
+
+    return result
+
+def tf_broadcaster(broadcast, link, parent_frame, child_frame, translation, rotation):
+
+    link.header.stamp = rospy.Time.now()
+    link.header.frame_id = parent_frame
+    link.child_frame_id = child_frame
+
+    link.transform.translation = translation
+
+    link.transform.rotation = rotation
+    broadcast.sendTransform(link)
+
+    return
+
+def transform_callback(msg):
+    global t 
+    t = msg.transforms
+
+t = FiducialTransformArray()
+rospy.init_node('static_tf')
+
+sub_topic_name ="/fiducial_transforms"
+transform_subscriber = rospy.Subscriber(sub_topic_name, FiducialTransformArray, transform_callback)
+
+rate = rospy.Rate(1)
+while not rospy.is_shutdown():
+    print(t)
+    rate.sleep()
+
+quat_msg = Quaternion(0.707,.0,0.0,0.707)
+trans_msg = Vector3(1.0, 0.0, 0.0)
+
+quat_init = Quaternion(0.4963, 0.4963, 0.0, -0.71236)
+trans_init = Vector3(0.8, 0.0, 0.5)
+
+
+broadcast = tf2_ros.StaticTransformBroadcaster()
+base_link = TransformStamped()
+
+tf_broadcaster(broadcast, base_link, "robot1", "camera_link", Vector3(-0.1,0.0,.0),Quaternion(.0,.0,.0,1.0))
+rospy.sleep(1)
+tf_broadcaster(broadcast, base_link, "robot1", "turtle", trans_init, quat_init)
+
+
+bigbroadcast = tf2_ros.StaticTransformBroadcaster()
+next_link = TransformStamped()
+
+tf_broadcaster(bigbroadcast, next_link, "robot1", "turtle1", trans_msg, quat_msg)
+rospy.sleep(1)
+
+quat_init_ar = quat_to_array(quat_init)
+quat_msg_ar = quat_to_array(quat_msg)
+
+quat_init_ar[3] = -quat_init_ar[3]
+
+f = array_to_quat(quat_init_ar)
+
+
+new_lin = TransformStamped()
+t1 = reverse_translate(trans_init)
+t2 = trans_msg
+t0 = Vector3(0,0,0)
+r0 = Quaternion(0,0,0,1)
+
+tf_broadcaster(broadcast, new_lin, "turtle", "b1", t0, f)
+tf_broadcaster(broadcast, new_lin, "b1", "b2", t1, r0)
+tf_broadcaster(broadcast, new_lin, "b2", "replacement", trans_msg, quat_msg)
+rospy.sleep(1)
+
+#another = TransformStamped()
+#tf_broadcaster(broadcast, new_lin, "back", "backagain", t1, n)
