14baaf4421de1fa73d2bebaab4ced55b8ac011c0
Shakif-a <84608878+Shakif-a@users.noreply.github.com>
2023-01-20 12:49:50 +1100

    Mean and std dev calculation from errors

--- src/quaternion_playground_v2.py
+++ src/quaternion_playground_v2.py
@@ -2,9 +2,10 @@
 
 # Just to have fun with quaternions, visualize them for my own understanding of how they work
 import rospy
+import numpy as np
 import tf2_ros
 from geometry_msgs.msg import Quaternion, Vector3, Transform, TransformStamped
-from fiducial_msgs.msg import FiducialTransformArray
+from fiducial_msgs.msg import FiducialTransformArray, FiducialTransform
 from tf.transformations import *
 
 def vector3_add(vector1, vector2):
@@ -57,18 +58,51 @@ def tf_broadcaster(broadcast, link, parent_frame, child_frame, translation, rota
 
 def transform_callback(msg):
     global t 
-    t = msg.transforms
 
-t = FiducialTransformArray()
+    global image_errors
+    global object_errors
+
+    t = msg.transforms
+    image_error = t[0].image_error
+    object_error = t[0].object_error
+    image_errors.append(image_error)
+    object_errors.append(object_error)
+
+def callback(event):
+    global image_errors
+    global object_errors
+
+    mean = np.mean(image_errors)
+    std_dev = np.std(image_errors)
+
+    mean2 = np.mean(object_errors)
+    std_dev2 = np.std(object_errors)
+
+    print("Mean is {:.2f}".format(mean))
+    print("Std dev is {:.2f} \n".format(std_dev))
+
+    print("O Mean is {:.2f}".format(mean2))
+    print("O Std dev is {:.2f}".format(std_dev2))
+
+    rospy.signal_shutdown("Shutting down ......")
+
 rospy.init_node('static_tf')
+t = [FiducialTransform() for i in range(2)]
+#check which is better to use - image or object error or fid area
+image_errors = []
+object_errors = []
+starting_time = rospy.Time.now()
+difference = rospy.Duration(1)
 
 sub_topic_name ="/fiducial_transforms"
 transform_subscriber = rospy.Subscriber(sub_topic_name, FiducialTransformArray, transform_callback)
 
-rate = rospy.Rate(1)
-while not rospy.is_shutdown():
-    print(t)
-    rate.sleep()
+timer = rospy.Timer(rospy.Duration(10),callback)
+
+# rate = rospy.Rate(1)
+# while not rospy.is_shutdown():
+#     print(t[0].image_error)
+#     rate.sleep()
 
 quat_msg = Quaternion(0.707,.0,0.0,0.707)
 trans_msg = Vector3(1.0, 0.0, 0.0)
@@ -109,6 +143,6 @@ tf_broadcaster(broadcast, new_lin, "turtle", "b1", t0, f)
 tf_broadcaster(broadcast, new_lin, "b1", "b2", t1, r0)
 tf_broadcaster(broadcast, new_lin, "b2", "replacement", trans_msg, quat_msg)
 rospy.sleep(1)
-
+rospy.spin()
 #another = TransformStamped()
 #tf_broadcaster(broadcast, new_lin, "back", "backagain", t1, n)
